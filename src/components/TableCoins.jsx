8717ed045f1f117ac813027107e9414473ccb39b
Caleb Parrra <mccalebyt@gmail.com>
2022-07-06 09:38:03 -0500

    'loading'

--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -4,10 +4,12 @@ const TableCoins = () => {
 
     const [coins, setCoins] = useState([]);
     const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(true);
 
     const getCoins = async () => {
         const response = await fetch('https://api.coingecko.com/api/v3/coins')
         const data = await response.json();
+        setLoading(false);
         setCoins(data);
         //  console.log(coins)
 
@@ -26,32 +28,38 @@ const TableCoins = () => {
 
     return (
         <div className="App bg-dark h-full">
-            <div className='mb-5'>
+            <div className='mb-5 text-center'>
                 <input value={search} onChange={searcher} type='text' placeholder='Search...' className='form-control-lg ' />
             </div>
-            
-            <div className='table-responsive'>
-                <table className='table text-white text-center table-sm align-middle'>
-                    <thead className='table-light '>
-                        <tr>
-                            <th>Ranking</th>
-                            <th>Name</th>
-                            <th>Symbol</th>
-                            <th>Price</th>
-                            <th>Price 24h</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            results.map((result) => {
-                                return (
-                                    <CoinRow key={result.id} result={result} />
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
+            {
+                loading ? (
+                    <h1 className='text-center'>Cargando...</h1>
+                ) : (
+                    <div className='table-responsive'>
+                        <table className='table text-white text-center table-sm align-middle'>
+                            <thead className='table-light '>
+                                <tr>
+                                    <th>Ranking</th>
+                                    <th>Name</th>
+                                    <th>Symbol</th>
+                                    <th>Price</th>
+                                    <th>Price 24h</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    results.map((result) => {
+                                        return (
+                                            <CoinRow key={result.id} result={result} />
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
+
 
         </div>
     )
